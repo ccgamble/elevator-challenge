966a1b56f4a1b36b61e605344994d78e82ddefd5
ccgamble <christine.c.gamble@gmail.com>
2017-02-15 06:08:51 -0700

    all tests passing for level4

--- elevator.js
+++ elevator.js
@@ -1,8 +1,52 @@
 export default class Elevator {
   constructor() {
+  this.state = 'idle';
+  this.currentFloor = 0;
+  this.riders = [];
+  this.requests = [];
+  this.totalRequests = 0;
+  this.stops = 0;
+  this.floors = 0;
   }
 
   reset() {
     this.constructor()
   }
+  
+  requestFloor(person, floor) {
+    this.requests.push({person: person.name, startFloor: person.currentFloor, reqFloor: floor})
+    
+    this.handleRequests()
+  
+  }
+  
+  
+  elevatorToPickup(request) {
+    let numOfFloors = Math.abs(this.currentFloor - request.startFloor)
+    this.floors += numOfFloors;
+    this.stops += 1;
+    this.riders +=1;
+    this.totalRequests += 1;
+    this.currentFloor = request.startFloor;
+  }
+  
+  elevatorToDropoff(request) {
+    let numOfFloors = Math.abs(request.startFloor - request.reqFloor)
+    this.floors += numOfFloors;
+    this.stops += 1;
+    this.riders -=1;
+    this.currentFloor = request.reqFloor;
+  }
+  
+  
+  handleRequests() {
+    this.elevatorToPickup(this.requests[0])
+    this.elevatorToDropoff(this.requests[0])
+    this.requests.shift()
+    if(this.requests.length > 0) {
+      this.handleRequests()
+    }
+  }
+
 }
+
--- tests/elevator_test.js
+++ tests/elevator_test.js
@@ -9,15 +9,19 @@ const Person = require('../person').default
 describe('Elevator', function() {
   const elevator = new Elevator()
   const alex = new Person("Alex", 2)
+  const bob = new Person("Bob", 3)
+  const sue = new Person("Sue", 6)
+
 
   afterEach(function() {
     elevator.reset();
   });
 
-  xit('should bring a rider to a floor above their current floor', () => {
+  it('should bring a rider to a floor above their current floor', () => {
+    
     // Alex requests the elevator to take him from 2 to 5
     elevator.requestFloor(alex, 5)
-
+  
     // Assert the current floor of the elevator is the drop off floor
     assert.equal(elevator.currentFloor, 5)
     // Assert the current status of the elevator is idle after drop off
@@ -28,7 +32,82 @@ describe('Elevator', function() {
     assert.equal(elevator.floors, 5)
   });
 
-  xit('should bring a rider to a floor below their current floor', () => {
+  it('should bring a rider to a floor below their current floor', () => {
+    elevator.requestFloor(alex, 1)
+    
+    assert.equal(elevator.currentFloor, 1)
+    assert.equal(elevator.state, 'idle')
+    assert.equal(elevator.stops, 2)
+    assert.equal(elevator.floors, 3)
+    assert.equal(elevator.riders, 0)
+    assert.equal(elevator.totalRequests, 1)
+    assert.equal(elevator.requests.length, 0)
   });
+  
+
+  it('should bring Bob and Sue to desired floors in order', () => {
+    elevator.requestFloor(bob, 9)
+    elevator.requestFloor(sue, 2)
+
+    assert.equal(elevator.currentFloor, 2)
+    assert.equal(elevator.state, 'idle')
+    assert.equal(elevator.stops, 4)
+    assert.equal(elevator.floors, 16)
+    assert.equal(elevator.riders, 0)
+    assert.equal(elevator.totalRequests, 2)
+    assert.equal(elevator.requests.length, 0)
+  });
+  
+  it('should bring both Bob and Sue up', () => {
+    elevator.requestFloor(bob, 5)
+    elevator.requestFloor(sue, 10)
+    
+    assert.equal(elevator.currentFloor, 10)
+    assert.equal(elevator.state, 'idle')
+    assert.equal(elevator.stops, 4)
+    assert.equal(elevator.floors, 10)
+    assert.equal(elevator.riders, 0)
+    assert.equal(elevator.totalRequests, 2)
+    assert.equal(elevator.requests.length, 0)
+  })
+  
+  it('should bring both Bob and Sue down', () => {
+    elevator.requestFloor(bob, 1)
+    elevator.requestFloor(sue, 3)
+    
+    assert.equal(elevator.currentFloor, 3)
+    assert.equal(elevator.state, 'idle')
+    assert.equal(elevator.stops, 4)
+    assert.equal(elevator.floors, 13)
+    assert.equal(elevator.riders, 0)
+    assert.equal(elevator.totalRequests, 2)
+    assert.equal(elevator.requests.length, 0)
+  })
+  
+  it('should bring Bob down and Sue up', () => {
+    elevator.requestFloor(bob, 1)
+    elevator.requestFloor(sue, 10)
+    
+    assert.equal(elevator.currentFloor, 10)
+    assert.equal(elevator.state, 'idle')
+    assert.equal(elevator.stops, 4)
+    assert.equal(elevator.floors, 14)
+    assert.equal(elevator.riders, 0)
+    assert.equal(elevator.totalRequests, 2)
+    assert.equal(elevator.requests.length, 0)
+  })
+  
+  it('should bring Bob up and Sue down', () => {
+    elevator.requestFloor(bob, 7)
+    elevator.requestFloor(sue, 2)
+    
+    assert.equal(elevator.currentFloor, 2)
+    assert.equal(elevator.state, 'idle')
+    assert.equal(elevator.stops, 4)
+    assert.equal(elevator.floors, 12)
+    assert.equal(elevator.riders, 0)
+    assert.equal(elevator.totalRequests, 2)
+    assert.equal(elevator.requests.length, 0)
+  })
 
 });
